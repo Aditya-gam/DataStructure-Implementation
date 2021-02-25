1189422baae124bd4d2bf67dc77b7e64fac65bef
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2021-02-25 10:41:43 +0530

    Added insertNode and print2D methods

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -31,14 +31,70 @@ class BST {
         }
     }
 
+    void insertNode(TreeNode new_node) {
+
+        if(root == null) {
+            root = new_node;
+            System.out.println("Value Inserted as root node");
+        }
+
+        else {
+            TreeNode temp = root;
+
+            while(temp != null) {
+
+                if(new_node.value == temp.value) {
+                    System.out.println("Value already exists, Enter another value");
+                    return;
+                }  
+                else if((new_node.value < temp.value) && (temp.left == null)) {
+                    temp.left = new_node;
+                    System.out.println("Value inserted to left");
+                    break;
+                }  
+                else if(new_node.value < temp.value) {
+                    temp = temp.left;
+                }
+                else if((new_node.value > temp.value) && (temp.right == null)) {
+                    temp.right = new_node;
+                    System.out.println("Value inserted to right");
+                    break;
+                }  
+                else if(new_node.value > temp.value) {
+                    temp = temp.right;
+                }
+
+
+            }
+        }
+    }
+
+    void print2D(TreeNode r, int space) {
+        if (r == null) {
+            return ;
+        }
+
+        space += 10;
+        print2D(r.right, space);
+        System.out.println();
+        for(int i = 10; i < space; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(r.value);
+        print2D(r.left, space);
+    }
+
     public static void main(String[] args) {
-        int opt;
+        BST obj = new BST();
+        int opt; 
+        int val;
         boolean loop = true;
         Scanner cin = new Scanner(System.in);
         while(loop) {
             System.out.print("1.Insert Node\n2.Search Node\n3.Delete Node\n4.Print BST Values\n0.Exit\nEnter your choice: ");
             opt = cin.nextInt();
-    
+            TreeNode new_node = new TreeNode();
+
             switch(opt) {
                 case 0:
                     loop = false;
@@ -46,6 +102,11 @@ class BST {
                 
                 case 1:
                     System.out.println("Insert");
+                    System.out.println("Enter value of tree node to insert in BST: ");
+                    val = cin.nextInt();
+                    new_node.value = val;
+                    obj.insertNode(new_node);
+                    System.out.println();
                     break;
     
                 case 2:
@@ -58,6 +119,7 @@ class BST {
     
                 case 4:
                     System.out.println("Print");
+                    obj.print2D(obj.root, 5);
                     break;
     
                 default:
