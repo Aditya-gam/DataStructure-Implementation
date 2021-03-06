3c9e8a52aeb82d495b4603ed6f1a7f04e4c14cb8
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2021-03-06 18:00:03 +0530

    Create AVLTree.java

---
+++ AVLTree.java
@@ -0,0 +1,17 @@
+class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+        value = 0;
+        left = null;
+        right = null;
+    }
+
+    public TreeNode(int v) {
+        value = v;
+        left = null;
+        right = null;
+    }
+};
